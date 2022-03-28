ca1bf929c1830459618a866753ee816529992133
Javierantonio092 <62306800+Javierantonio092@users.noreply.github.com>
2022-03-27 22:51:27 -0600

    feat: layout admin

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -12,6 +12,7 @@ module.exports = app =>{
     router.post("/auth/signup", auth.signUp);
     router.get("/auth/logout", auth.logout);
     router.get("/profile", auth.profile);
+    router.get("/indexAdmin", auth.indexAdmin);
 
     app.use(router);
 };
